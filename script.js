3642504a0391d5c933ca344a9284c157b10b2daa
Timothy <128176649+Timothy-P@users.noreply.github.com>
2023-11-07 16:05:23 +0000

    Testing

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
     $("#canvas1").click(Alpha());
 });
+/*$("body").mousemove(function(e) {
+    document.Form1.posx.value = e.pageX;
+    document.Form1.posy.value = e.pageY;
+})*/
 function Alpha() {
     let canvas = document.getElementById("canvas1");
     let ctx = canvas.getContext("2d");
     const circle = new Path2D();
-    circle.arc(e.clientX, e.clientY, 25, 0, 2 * Math.PI);
+    circle.arc(clientX, clientY, 25, 0, 2 * Math.PI);
 };
 //onmousemove = function(e){console.log("mouse location:", e.clientX, e.clientY)}
