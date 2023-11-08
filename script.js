068364179189ef215b3555ef7dc4aeeac5dfd4b1
Timothy <128176649+Timothy-P@users.noreply.github.com>
2023-11-08 16:12:36 +0000

    It won't stop triggering on start. DX

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let tempB;
 let tempC;
 let tempD;
 var counter = "0";
+var temp1 = 0
 
 onmousemove = function(e){
     mouseX = eval(e.clientX+"-30")
@@ -16,8 +17,9 @@ onmousemove = function(e){
 }
 function Alpha() { 
     try {
+        if (temp1 == 0){
         ColorRan = Math.floor(Math.random()*16777215).toString(16);
-        $("#canvas1").append('<div id="dot'+counter+'" style="left:'+mouseX+'px;top:'+mouseY+'px; width:60px; height:60px;background-color:#'+ColorRan+';border-radius:100px;position:absolute;"></div>');
+        $("#canvas1").append('<div id="dot'+counter+'" style="left:'+mouseX+'px;top:'+mouseY+'px; width:60px; height:60px;background-color:#'+ColorRan+';border-radius:100px;position:absolute;border-color:transparent"></div>');
         tempA = document.getElementById("dot"+counter).getAttribute("style")
         counter = eval(counter+1);
         tempB = tempA.indexOf("l");
@@ -26,6 +28,11 @@ function Alpha() {
         //alert("Dot created at "+mouseX+", "+mouseY+" according to the mouse");
         //alert("Dot created at "+tempD+" according to the element");
         err = Error
+        //document.getElementById("dot0").remove()
+        }
+        else {
+            temp1 = 1
+        }
     }
     catch (err) {
         var myWindow = window.open("", "MsgWindow", "width=300,height=25");
